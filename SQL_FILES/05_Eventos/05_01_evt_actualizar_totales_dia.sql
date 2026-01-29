fe8a823f89413503c134421568b3b5cd1971c1dc
Andres Vasquez <andres@thenextsecurity.cl>
2026-01-29 12:31:11 -0300

    feat(database): agregar evento para actualización automática de totales diarios
    Se ha creado un nuevo evento SQL llamado `event_actualizar_totales_dia`, que se ejecuta cada 30 minutos para procesar y actualizar los totales del día. El evento incluye manejo de errores y registros de auditoría para asegurar la trazabilidad de las operaciones realizadas. Estos cambios buscan mejorar la automatización y la gestión de datos en la base de datos.
    
    Issue: #1

---
+++ SQL_FILES/05_Eventos/05_01_evt_actualizar_totales_dia.sql
@@ -0,0 +1,44 @@
+CREATE EVENT event_actualizar_totales_dia
+ON SCHEDULE EVERY 30 MINUTE
+STARTS '2025-01-28 00:00:00.000'
+ON COMPLETION NOT PRESERVE
+ENABLE
+DO BEGIN
+    DECLARE v_success_dia BOOLEAN DEFAULT FALSE;
+    DECLARE v_dia_actual DATE;
+    DECLARE v_error_message TEXT;
+    
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        GET DIAGNOSTICS CONDITION 1 v_error_message = MESSAGE_TEXT;
+        INSERT INTO sem_auditoria_detallada (
+            tipo_operacion, tabla_afectada, usuario, datos_nuevos, 
+            fecha_operacion, direccion_ip, aplicacion
+        ) VALUES (
+            'ERROR', 'sem_totales_dia', 'SYSTEM',
+            JSON_OBJECT(
+                'mensaje', 'Error en actualización de totales día',
+                'error', v_error_message,
+                'hora_ejecucion', NOW()
+            ),
+            CURRENT_TIMESTAMP(6), '127.0.0.1', 'EVENT_actualizar_totales_dia'
+        );
+    END;
+
+    SET v_dia_actual = CURRENT_DATE();
+    SET v_success_dia = fn_process_totales_dia(v_dia_actual);
+
+    IF v_success_dia THEN
+        INSERT INTO sem_auditoria_detallada (
+            tipo_operacion, tabla_afectada, usuario, datos_nuevos, 
+            fecha_operacion, direccion_ip, aplicacion
+        ) VALUES (
+            'ACTUALIZACION', 'sem_totales_dia', 'SYSTEM',
+            JSON_OBJECT(
+                'mensaje', 'Actualización exitosa de totales día',
+                'dia_procesado', v_dia_actual
+            ),
+            CURRENT_TIMESTAMP(6), '127.0.0.1', 'EVENT_actualizar_totales_dia'
+        );
+    END IF;
+END
